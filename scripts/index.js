f77d8a745dd598855c540a70e717028683089fc3
Vladislav Zakazchikov <vs.zakazchikov@gmail.com>
2024-12-29 16:59:45 +0300

    Поправка в файле скрипта

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,24 @@
-const templateCard = document.querySelector('#card-template').content;
-const placesContent = document.querySelector('.places');
-const placesList = placesContent.querySelector('.places__list')
+const templateCard = document.querySelector("#card-template").content;
+const placesContent = document.querySelector(".places");
+const placesList = placesContent.querySelector(".places__list");
 
 function createCard(element, deleteCard) {
-    const templateCardCopy = templateCard.cloneNode('true');
-    templateCardCopy.querySelector('.card__title').textContent = element.name;
-    templateCardCopy.querySelector('.card__image').src = element.link;
-    templateCardCopy.querySelector('.card__image').src = element.link;
-    templateCardCopy.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-    return templateCardCopy;
+  const templateCardCopy = templateCard.cloneNode("true");
+  templateCardCopy.querySelector(".card__title").textContent = element.name;
+  templateCardCopy.querySelector(".card__image").src = element.link;
+  templateCardCopy.querySelector(".card__image").src = element.link;
+  templateCardCopy
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+  return templateCardCopy;
 }
 
-function deleteCard(element){
-    const deleteCard = element.target.closest('.card');
-    deleteCard.remove();
-};
+function deleteCard(element) {
+  const deleteCard = element.target.closest(".card");
+  deleteCard.remove();
+}
 
-initialCards.forEach(function(element){
-    const cardItem = createCard(element, deleteCard)
-    placesList.append(cardItem); 
+initialCards.forEach(function (element) {
+  const cardItem = createCard(element, deleteCard);
+  placesList.append(cardItem);
 });
