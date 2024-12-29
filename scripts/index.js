c2888454d31d021da816b4b882e083adf49add87
Vladislav Zakazchikov <vs.zakazchikov@gmail.com>
2024-12-29 20:42:15 +0300

    Поправка в файле скрипта

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ function createCard(element, deleteCard) {
   const templateCardCopy = templateCard.cloneNode("true");
   templateCardCopy.querySelector(".card__title").textContent = element.name;
   templateCardCopy.querySelector(".card__image").src = element.link;
-  templateCardCopy.querySelector(".card__image").src = element.link;
+  templateCardCopy.querySelector(".card__image").alt = element.link;
   templateCardCopy
     .querySelector(".card__delete-button")
     .addEventListener("click", deleteCard);
@@ -14,7 +14,7 @@ function createCard(element, deleteCard) {
 }
 
 function deleteCard(element) {
-  const deleteCard = element.target.closest(".card");
+  const deleteCard = element.target.closest(".card");``
   deleteCard.remove();
 }
 
