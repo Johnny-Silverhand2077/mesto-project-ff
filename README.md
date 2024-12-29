7a7526e4cbcc16f1ca1cabb538ecb744d058e60c
Vladislav Zakazchikov <vs.zakazchikov@gmail.com>
2024-12-29 16:56:27 +0300

    Реализованна функции добавление карточек при загрузке страници Реализованна функция удаление карточек изменение файла README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+* Мой репозиторий https://github.com/Johnny-Silverhand2077/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const templateCard = document.querySelector('#card-template').content;
+const placesContent = document.querySelector('.places');
+const placesList = placesContent.querySelector('.places__list')
 
-// @todo: DOM узлы
+function createCard(element, deleteCard) {
+    const templateCardCopy = templateCard.cloneNode('true');
+    templateCardCopy.querySelector('.card__title').textContent = element.name;
+    templateCardCopy.querySelector('.card__image').src = element.link;
+    templateCardCopy.querySelector('.card__image').src = element.link;
+    templateCardCopy.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return templateCardCopy;
+}
 
-// @todo: Функция создания карточки
+function deleteCard(element){
+    const deleteCard = element.target.closest('.card');
+    deleteCard.remove();
+};
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function(element){
+    const cardItem = createCard(element, deleteCard)
+    placesList.append(cardItem); 
+});
