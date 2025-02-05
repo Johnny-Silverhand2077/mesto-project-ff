d53a3589378aedcfb43bca0f4ed94b764425026e
Vladislav Zakazchikov <vs.zakazchikov@gmail.com>
2025-02-05 16:38:57 +0300

    Добавление ведпака

---
+++ src/components/card.js
@@ -0,0 +1,21 @@
+const templateCard = document.querySelector("#card-template").content;
+const placesContent = document.querySelector(".places");
+const placesList = placesContent.querySelector(".places__list");
+ 
+ function createCard(element, deleteCard) {
+    const templateCardCopy = templateCard.cloneNode("true");
+    templateCardCopy.querySelector(".card__title").textContent = element.name;
+    templateCardCopy.querySelector(".card__image").src = element.link;
+    templateCardCopy.querySelector(".card__image").alt = element.link;
+    templateCardCopy
+      .querySelector(".card__delete-button")
+      .addEventListener("click", deleteCard);
+    return templateCardCopy;
+  }
+
+ function deleteCard(element) {
+    const deleteCard = element.target.closest(".card");
+    deleteCard.remove();
+  }
+
+export {placesList, createCard, deleteCard}
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './styles/index.css';
+import {initialCards} from './cards.js'
+import {createCard, deleteCard, placesList} from './components/card.js';
+
+
+
+
+// const templateCard = document.querySelector("#card-template").content;
+// const placesContent = document.querySelector(".places");
+// const placesList = placesContent.querySelector(".places__list");
+
+// function createCard(element, deleteCard) {
+//   const templateCardCopy = templateCard.cloneNode("true");
+//   templateCardCopy.querySelector(".card__title").textContent = element.name;
+//   templateCardCopy.querySelector(".card__image").src = element.link;
+//   templateCardCopy.querySelector(".card__image").alt = element.link;
+//   templateCardCopy
+//     .querySelector(".card__delete-button")
+//     .addEventListener("click", deleteCard);
+//   return templateCardCopy;
+// }
+
+// function deleteCard(element) {
+//   const deleteCard = element.target.closest(".card");``
+//   deleteCard.remove();
+// }
+
+initialCards.forEach(function (element) {
+  const cardItem = createCard(element, deleteCard);
+  placesList.append(cardItem);
+});
+
