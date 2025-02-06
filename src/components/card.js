e57963035c78d1f784bd00bc71cce4aa025fe01c
Vladislav Zakazchikov <vs.zakazchikov@gmail.com>
2025-02-06 14:48:29 +0300

    Закончена работа над модальными окнами добавлена плавность модальным окнам при открытии и закрытии

--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,58 @@
+import { initialCards } from "../cards";
+import { handleLikeClick } from "./modal";
+import { openModal } from "./modals";
+
 const templateCard = document.querySelector("#card-template").content;
 const placesContent = document.querySelector(".places");
 const placesList = placesContent.querySelector(".places__list");
- 
- function createCard(element, deleteCard) {
-    const templateCardCopy = templateCard.cloneNode("true");
-    templateCardCopy.querySelector(".card__title").textContent = element.name;
-    templateCardCopy.querySelector(".card__image").src = element.link;
-    templateCardCopy.querySelector(".card__image").alt = element.link;
-    templateCardCopy
-      .querySelector(".card__delete-button")
-      .addEventListener("click", deleteCard);
-    return templateCardCopy;
-  }
+const popupImage = document.querySelector(".popup_type_image");
 
- function deleteCard(element) {
-    const deleteCard = element.target.closest(".card");
-    deleteCard.remove();
-  }
+function createCard(element, deleteCard, handleLikeClick, handleOpenImage) {
+  const templateCardCopy = templateCard.cloneNode("true");
+  const cardImage = templateCardCopy.querySelector(".card__image");
+  const cardTitle = templateCardCopy.querySelector(".card__title");
+  const deleteButton = templateCardCopy.querySelector(".card__delete-button");
+  const likeButton = templateCardCopy.querySelector(".card__like-button");
 
-export {placesList, createCard, deleteCard}
+  cardTitle.textContent = element.name;
+  cardImage.src = element.link;
+  cardImage.alt = element.alt;
+
+  cardImage.addEventListener("click", () =>
+    handleOpenImage(cardImage.src, cardTitle.textContent)
+  );
+
+  deleteButton.addEventListener("click", deleteCard);
+  likeButton.addEventListener("click", handleLikeClick);
+
+  return templateCardCopy;
+}
+
+function deleteCard(element) {
+  const deleteCard = element.target.closest(".card");
+  deleteCard.remove();
+}
+
+initialCards.forEach(function (element) {
+  const cardItem = createCard(
+    element,
+    deleteCard,
+    handleLikeClick,
+    handleOpenImage
+  );
+  placesList.append(cardItem);
+});
+
+function handleOpenImage(imageLink, imageName) {
+  const imageElement = document.querySelector(".popup__image");
+  const captionElement = document.querySelector(".popup__caption");
+
+  imageElement.src = imageLink;
+  imageElement.alt = imageName;
+
+  captionElement.textContent = imageName;
+
+  openModal(popupImage);
+}
+
+export { placesList, createCard, deleteCard, handleOpenImage };
